5527b1e326fb1d4837126e39c91302451cb70b64
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-08 20:09:21 -0400

    Pushing up with serverless.yml file issue during early deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const sls = require("serverless-http");
 const binaryMimeTypes = require("./binaryMimeTypes");
 
 const server = require("./server");
+
 module.exports.server = sls(server, {
   binary: binaryMimeTypes
 });
